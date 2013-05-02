e33ba6aec46e1663963df2d51fb030784b88281e
Bruno Rocha <rochacbruno@gmail.com>
2013-05-01 22:08:39 -0300

    changed init

--- opps/__init__.py
+++ opps/__init__.py
@@ -1,5 +1,5 @@
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pkg_resources
+
+pkg_resources.declare_namespace(__name__)
