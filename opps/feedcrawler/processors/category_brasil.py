28998351c720df577aa8426a8c607cf887b7da34
Bruno Rocha <rochacbruno@gmail.com>
2013-09-14 18:35:11 -0300

    categorization

--- opps/feedcrawler/processors/category_brasil.py
+++ opps/feedcrawler/processors/category_brasil.py
@@ -1,14 +1,14 @@
 CATEGORY_BRASIL = {
     u'http://agenciabrasil.ebc.com.br/feed/assunto/55/all/feed.xml': u'noticias/brasil',
     u'http://agenciabrasil.ebc.com.br/feed/assunto/51/all/feed.xml': u'noticias/economia',
-    u'http://agenciabrasil.ebc.com.br/feed/assunto/62/all/feed.xml': u'noticias/educacao',
+    u'http://agenciabrasil.ebc.com.br/feed/assunto/62/all/feed.xml': u'noticias/comportamento-educacao',
     u'http://agenciabrasil.ebc.com.br/feed/assunto/58/all/feed.xml': u'noticias/brasil',
-    u'http://agenciabrasil.ebc.com.br/feed/assunto/85/all/feed.xml': u'noticias/meio-ambiente',
+    u'http://agenciabrasil.ebc.com.br/feed/assunto/85/all/feed.xml': u'noticias/ciencia-tecnologia/meio-ambiente',
     u'http://agenciabrasil.ebc.com.br/feed/assunto/85002/all/feed.xml': u'noticias/mundo',
     u'http://agenciabrasil.ebc.com.br/feed/assunto/45/all/feed.xml': u'noticias/brasil/politica',
     u'http://agenciabrasil.ebc.com.br/feed/assunto/57/all/feed.xml': u'noticias/saude',
     u'http://agenciabrasil.ebc.com.br/feed/assunto/55343/all/feed.xml': u'noticias/brasil',
     u'http://agenciabrasil.ebc.com.br/feed/assunto/377/all/feed.xml': u'esportes',
-    u'http://www.ebc.com.br/rss/cultura/feed.xml': u'cultura',
+    u'http://www.ebc.com.br/rss/cultura/feed.xml': u'entretenimento/cultura',
     u'http://www.ebc.com.br/rss/tecnologia/feed.xml': u'noticias/ciencia-tecnologia'
 }
