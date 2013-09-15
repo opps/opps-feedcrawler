4c552309359ea0f4be3360b53640ac66bcfc322d
Bruno Rocha <rochacbruno@gmail.com>
2013-09-15 00:07:26 -0300

    changed perioric task

--- opps/feedcrawler/tasks.py
+++ opps/feedcrawler/tasks.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 import celery
-from celery.task.schedules import crontab
+# from celery.task.schedules import crontab
+from django.utils import timezone
 from opps.feedcrawler.management.commands.process_feeds import Command
 
-@celery.task(run_every=crontab(hour="*", minute="30", day_of_week="*"))
+# @celery.task(run_every=crontab(hour="*", minute="30", day_of_week="*"))
+@celery.task.periodic_task(run_every=timezone.timedelta(minutes=20))
 def process_feeds():
     command = Command()
     command.handle()
