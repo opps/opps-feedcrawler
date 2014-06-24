d3f4bbda0ff9102b2e6317e77df0a0cda951076f
Igor P. Leroy <ip.leroy@gmail.com>
2014-06-24 17:18:23 -0300

    Update version

--- opps/feedcrawler/__init__.py
+++ opps/feedcrawler/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 
 pkg_resources.declare_namespace(__name__)
 
-VERSION = (0, 2, 0)
+VERSION = (0, 2, 1)
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
@@ -13,5 +13,5 @@ __description__ = u"Feed reader App for Opps CMS"
 __author__ = u"Bruno Cezar Rocha"
 __credits__ = []
 __email__ = u"rochacbruno@gmail.com"
-__license__ = u"MIT License"
+__license__ = u"MIT"
 __copyright__ = u"Copyright 2013, YACOWS"
